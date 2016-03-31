e37afa3627237a64a03058f7cced07e2d66384b8
Ryan Willoughby <ryan.willoughby@adobe.com>
2016-03-31 15:01:02 -0700

    [android] updating to support android sdk 23

--- src/android/src/com/phonegap/build/oauth/PhonegapBuildOauth.java
+++ src/android/src/com/phonegap/build/oauth/PhonegapBuildOauth.java
@@ -1,23 +1,20 @@
 package com.phonegap.build.oauth;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.cordova.*;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.*;
 
 import android.util.Base64;
@@ -71,20 +68,25 @@ public class PhonegapBuildOauth extends CordovaPlugin {
 				+ Base64.encodeToString(
 						(username + ":" + password).getBytes(),
 						Base64.NO_WRAP);
-		
-		String json = postData(HOSTNAME + "/token", base64EncodedCredentials, null);
 
-		if (json != null) {
-			JSONObject obj;
-			try {
+		try {
+			String json = postData(HOSTNAME + "/token", base64EncodedCredentials, null);
+	
+			if (json != null) {
+				JSONObject obj;
 				obj = new JSONObject(json);
 				String token = obj.getString("token");
 				Log.d("PhonegapBuildOauth", token);
 				authorize(token);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail(e.getMessage());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail(e.getMessage());
 		}
 	}
 	
@@ -95,23 +97,22 @@ public class PhonegapBuildOauth extends CordovaPlugin {
 	}
 	
 	private void authorize(String token) {
-		List<NameValuePair> data = new ArrayList<NameValuePair>(3);
-		data.add(new BasicNameValuePair("client_id", CLIENT_ID));
-		data.add(new BasicNameValuePair("client_secret", CLIENT_SECRET));
-		data.add(new BasicNameValuePair("auth_token", token));
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("client_id", CLIENT_ID);
+		hm.put("client_secret", CLIENT_SECRET);
+		hm.put("auth_token", token);
 		try {
-			UrlEncodedFormEntity formData = new UrlEncodedFormEntity(data);
-			String json = postData(HOSTNAME + "/authorize", null, formData);
+			String json = postData(HOSTNAME + "/authorize", null, hm);
 			Log.d("PhonegapBuildOauth", json);
 			JSONObject obj = new JSONObject(json);
 			PluginResult r = new PluginResult(PluginResult.Status.OK, obj);
 			r.setKeepCallback(false);
 			cb.sendPluginResult(r);
-		} catch (UnsupportedEncodingException e) {
+		}  catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			fail(e.getMessage());
-		} catch (JSONException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			fail(e.getMessage());
@@ -119,86 +120,90 @@ public class PhonegapBuildOauth extends CordovaPlugin {
 	}
 	
 	private void authorizeByCode(String code) {
-		List<NameValuePair> data = new ArrayList<NameValuePair>(3);
-		data.add(new BasicNameValuePair("client_id", CLIENT_ID));
-		data.add(new BasicNameValuePair("client_secret", CLIENT_SECRET));
-		data.add(new BasicNameValuePair("code", code));
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("client_id", CLIENT_ID);
+		hm.put("client_secret", CLIENT_SECRET);
+		hm.put("code", code);
 		try {
-			UrlEncodedFormEntity formData = new UrlEncodedFormEntity(data);
-			String json = postData(HOSTNAME + "/authorize/token", null, formData);
+			String json = postData(HOSTNAME + "/authorize/token", null, hm);
 			JSONObject obj = new JSONObject(json);
 			PluginResult r = new PluginResult(PluginResult.Status.OK, obj);
 			r.setKeepCallback(false);
 			cb.sendPluginResult(r);
-		} catch (UnsupportedEncodingException e) {
+		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			fail(e.getMessage());
-		} catch (JSONException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
 	}
 
-	private String postData(String url, String creds, UrlEncodedFormEntity data) {
-		// Create a new HttpClient and Post Header
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpPost httppost = new HttpPost(url);
-		HttpResponse response = null;
-		StringBuilder total = new StringBuilder();
-
-		try {
-			if (creds != null) {
-				httppost.setHeader("Authorization", creds);
-			}
-
-			if (data != null) {
-				httppost.setEntity(data);
-			}
-			// Execute HTTP Post Request
-			response = httpclient.execute(httppost);
-
-		} catch (ClientProtocolException e) {
-			Log.e("PhonegapBuildOauth", "Uncaught exception from plugin", e);
-			e.printStackTrace();
-			fail(e.getMessage());
-		} catch (IOException e) {
-			Log.e("PhonegapBuildOauth", "Uncaught exception from plugin", e);
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-
-		HttpEntity entity = response.getEntity();
-
-		if (entity != null) {
-			try {
-				InputStream instream = entity.getContent();
-
-				String line = "";
+	private String postData(String url, String creds, HashMap<String, String> postDataParams) throws IOException {
+		
+		URL urlObj = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+        try {
+	        if (creds != null) {
+	        	conn.setRequestProperty("Authorization", creds);
+	        }
+	        conn.setDoOutput(true);
+	        conn.setChunkedStreamingMode(0);
+	
+	        OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+            out.write( getPostDataString(postDataParams).getBytes() );
+            out.flush();
+            out.close();
+            
+	        int rc = conn.getResponseCode();
+            if (rc == HttpURLConnection.HTTP_OK)
+            {
+                //no http response code er xror
+                //read the result from the server
+            	StringBuilder total = new StringBuilder();
+            	InputStream in = new BufferedInputStream(conn.getInputStream());
+            	String line = "";
 
 				// Wrap a BufferedReader around the InputStream
 				BufferedReader rd = new BufferedReader(new InputStreamReader(
-						instream));
+						in));
 
 				// Read response until the end
 				while ((line = rd.readLine()) != null) {
 					total.append(line);
 				}
 				Log.d("PhonegapBuildOauth", total.toString());
-				
+				in.close();
 				return total.toString();
-
-			} catch (IllegalStateException e) {
-				e.printStackTrace();
-				fail(e.getMessage());
-			} catch (IOException e) {
-				e.printStackTrace();
-				fail(e.getMessage());
-			}
-		}
-		
-		return null;
+            }
+            else
+            {
+                fail("http response code error: "+rc+"\n");
+            }
+        }
+        finally {
+        	conn.disconnect();
+        }
+        return null;
 	}
+	
+	private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
 
 }
